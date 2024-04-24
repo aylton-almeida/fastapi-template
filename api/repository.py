87c62ce25e1d428d2ef56832d567c72019a6d8ad
Aylton Almeida <almeida@aylton.dev>
2024-04-24 14:57:57 -0300

    📌 Migrated

--- api/repository.py
+++ api/repository.py
@@ -1,18 +1,20 @@
 import os
 from functools import lru_cache
 from typing import Iterator, List, Optional
+from contextlib import asynccontextmanager
 
 from pydantic import BaseModel
-from sqlalchemy import Boolean, Column, Integer, String, create_engine
+from sqlalchemy import Boolean, Column, Integer, String, create_async_engine
 from sqlalchemy.exc import DatabaseError
-from sqlalchemy.orm import Session, declarative_base, sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_session
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 SQL_BASE = declarative_base()
 
 
 @lru_cache(maxsize=None)
 def get_engine(db_string: str):
-    return create_engine(db_string.replace("+asyncpg", ""), pool_pre_ping=True)
+    return create_async_engine(db_string, pool_pre_ping=True)
 
 
 class TodoInDB(SQL_BASE):  # type: ignore
@@ -38,33 +40,33 @@ class TodoFilter(BaseModel):
 
 
 class TodoRepository:  # Interface
-    def __enter__(self):
+    async def __aenter__(self):
         return self
 
-    def __exit__(self, exc_type, exc_val, exc_tb):
+    async def __aexit__(self, exc_type, exc_val, exc_tb):
         pass
 
-    def save(self, todo: Todo) -> None:
+    async def save(self, todo: Todo) -> None:
         raise NotImplementedError()
 
-    def get_by_key(self, key: str) -> Optional[Todo]:
+    async def get_by_key(self, key: str) -> Optional[Todo]:
         raise NotImplementedError()
 
-    def get(self, todo_filter: TodoFilter) -> List[Todo]:
+    async def get(self, todo_filter: TodoFilter) -> List[Todo]:
         raise NotImplementedError()
 
 
-class InMemoryTodoRepository:  # In-memory implementation of interface
+class InMemoryTodoRepository(TodoRepository):  # In-memory implementation of interface
     def __init__(self):
         self.data = {}
 
-    def save(self, todo: Todo) -> None:
+    async def save(self, todo: Todo) -> None:
         self.data[todo.key] = todo
 
-    def get_by_key(self, key: str) -> Optional[Todo]:
+    async def get_by_key(self, key: str) -> Optional[Todo]:
         return self.data.get(key)
 
-    def get(self, todo_filter: TodoFilter) -> List[Todo]:
+    async def get(self, todo_filter: TodoFilter) -> List[Todo]:
         all_matching_todos = filter(
             lambda todo: (not todo_filter.key_contains or todo_filter.key_contains in todo.key)
             and (not todo_filter.value_contains or todo_filter.value_contains in todo.value)
@@ -77,31 +79,31 @@ class InMemoryTodoRepository:  # In-memory implementation of interface
 
 class SQLTodoRepository(TodoRepository):  # SQL Implementation of interface
     def __init__(self, session):
-        self._session: Session = session
+        self._session: AsyncSession = session
 
-    def __exit__(self, exc_type, exc_value: str, exc_traceback: str) -> None:
+    async def __aexit__(self, exc_type, exc_value: str, exc_traceback: str) -> None:
         if any([exc_type, exc_value, exc_traceback]):
-            self._session.rollback()
+            await self._session.rollback()
             return
 
         try:
-            self._session.commit()
+            await self._session.commit()
         except DatabaseError as e:
-            self._session.rollback()
+            await self._session.rollback()
             raise e
 
-    def save(self, todo: Todo) -> None:
+    async def save(self, todo: Todo) -> None:
         self._session.add(TodoInDB(key=todo.key, value=todo.value))
 
-    def get_by_key(self, key: str) -> Optional[Todo]:
-        instance = self._session.query(TodoInDB).filter(TodoInDB.key == key).first()
+    async def get_by_key(self, key: str) -> Optional[Todo]:
+        instance = await self._session.query(TodoInDB).filter(TodoInDB.key == key).first()
 
         if instance:
             return Todo(key=instance.key, value=instance.value, done=instance.done)
 
         return None
 
-    def get(self, todo_filter: TodoFilter) -> List[Todo]:
+    async def get(self, todo_filter: TodoFilter) -> List[Todo]:
         query = self._session.query(TodoInDB)
 
         if todo_filter.key_contains is not None:
@@ -119,14 +121,16 @@ class SQLTodoRepository(TodoRepository):  # SQL Implementation of interface
         return [Todo(key=todo.key, value=todo.value, done=todo.done) for todo in query]
 
 
-def create_todo_repository() -> Iterator[TodoRepository]:
-    session = sessionmaker(bind=get_engine(os.getenv("DB_STRING")))()
-    todo_repository = SQLTodoRepository(session)
+@asynccontextmanager
+async def create_todo_repository() -> Iterator[TodoRepository]:
+    async with create_async_session(get_engine(os.getenv("DB_STRING"))) as session:
+        todo_repository = SQLTodoRepository(session)
 
-    try:
-        yield todo_repository
-    except Exception:
-        session.rollback()
-        raise
-    finally:
-        session.close()
+        try:
+            yield todo_repository
+        except Exception:
+            await session.rollback()
+            raise
+        finally:
+            await session.close()
+
